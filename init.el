a3f83214ceeedec8cd77ea2eb0fc7bd4e9164565
Kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-02-25 05:02:51 +0900

    packageを正しい場所に書いていなかったので修正した

--- init.el
+++ init.el
@@ -16,255 +16,268 @@
     (leaf hydra :ensure t)
     (leaf el-get :ensure t)
     (leaf blackout :ensure t)
-    
+    (leaf beacon
+      :ensure t
+      :config (beacon-mode 1) (setq beacon-color "green"))
+
+    (leaf skk
+      :ensure ddskk
+      :custom ((default-input-method . "japanese-skk"))
+      :config
+      (leaf ddskk-posframe
+        :ensure t
+        :global-minor-mode t))
     :config
     ;; initialize leaf-keywords.el
-    (leaf-keywords-init)))
-;; </leaf-install-code>
+    (leaf-keywords-init)
 
-;; Now you can use leaf!
-(leaf leaf-tree :ensure t)
-(leaf leaf-convert :ensure t)
-(leaf transient-dwim
-  :ensure t
-  :bind (("M-=" . transient-dwim-dispatch)))
+    (leaf evil
+      :doc "https://fnwiya.hatenablog.com/entry/2016/01/12/213149"
+      :ensure t
+      :config   (setq evil-cross-lines t)           ;行の端でhlしたとき前/次の行に移動する
+      (setq evil-want-C-i-jump nil)       ;C-iはTABとして使う
+      (setq evil-search-module 'isearch)
+      (evil-mode 1)
+      (evil-set-initial-state 'org-mode 'emacs)
+      ;; (setq evil-normal-state-cursor '(box "purple"))
+      ;; (setq evil-emacs-state-cursor '(bar "green"))
+      ;; (setq evil-insert-state-cursor '(bar "green"))
+      ;; (setq evil-want-fine-undo t)     ;操作を元に戻す単位を細かくする
+      (setq evil-move-cursor-back nil) ;改行文字の上に移動可能にする(C-x C-e用)
+      (setq evil-esc-delay 0)
+      (defalias 'evil-insert-state 'evil-emacs-state)
+      (define-key evil-emacs-state-map (kbd "<escape>") 'evil-normal-state)
+      (add-hook 'evil-emacs-state-entry-hook 'skk-mode)
+      (add-hook 'evil-emacs-state-exit-hook 'skk-mode)
+      )
+    (leaf evil-matchit
+      :ensure t
+      :config (global-evil-matchit-mode 1))
+    (leaf evil-leader
+      :ensure t
+      :config
+      ;; evil leader
+      (setq evil-leader/in-all-states 1)
+      (global-evil-leader-mode)
+      (evil-leader/set-leader "SPC")
+      (evil-leader/set-key
+        "SPC" 'execute-extended-command
+        ":" 'shell-command
+        "TAB" (lambda() (interactive)(other-window 1))
+        ;; "a" 'avy-goto-word-0
+        "b" 'counsel-ibuffer
+        "B" 'byte-compile-file
+        "C" (lambda() (interactive)(find-file "~/.emacs.d/init.el"))
+        "d" 'dired-jump
+        ;; "f" 'projectile-find-file
+        "f" 'counsel-find-file
+        "g" 'magit-status
+        "h" 'lsp-describe-thing-at-point
+        "j" 'skk-mode
+        "k" 'kill-buffer
+        "m" 'magit-status
+        "q" 'kill-buffer-and-window
+        "Q" (lambda () (interactive) (save-buffers-kill-emacs))
+        "r" 'helm-recentf
+        "s" 'eshell
+        "S" 'shell
+        "t" 'neotree-toggle
+        "T" 'treemacs
+        "w" 'save-buffer
+        "y" 'yas-insert-snippet
+        ;; "q" 'save-buffer-kill-terminal
+        "^" (lambda () (interactive)
+              (show-org-buffer)
+              (evil-normal-state))
+        ";" 'comment-line
+        "x" 'toggle-transparency
+        "0" 'delete-window
+        "1" 'delete-other-windows
+        "2" 'split-window-below
+        "3" 'split-window-right
+        "9" 'create-scratch-buffer
+        ))
 
-(leaf evil
-  :doc "https://fnwiya.hatenablog.com/entry/2016/01/12/213149"
-  :ensure t
-  :config   (setq evil-cross-lines t)           ;行の端でhlしたとき前/次の行に移動する
-  (setq evil-want-C-i-jump nil)       ;C-iはTABとして使う
-  (setq evil-search-module 'isearch)
-  (evil-mode 1)
-  (evil-set-initial-state 'org-mode 'emacs)
-  ;; (setq evil-normal-state-cursor '(box "purple"))
-  ;; (setq evil-emacs-state-cursor '(bar "green"))
-  ;; (setq evil-insert-state-cursor '(bar "green"))
-  ;; (setq evil-want-fine-undo t)     ;操作を元に戻す単位を細かくする
-  (setq evil-move-cursor-back nil) ;改行文字の上に移動可能にする(C-x C-e用)
-  (setq evil-esc-delay 0)
-  (defalias 'evil-insert-state 'evil-emacs-state)
-  (define-key evil-emacs-state-map (kbd "<escape>") 'evil-normal-state)
-  (add-hook 'evil-emacs-state-entry-hook 'skk-mode)
-  (add-hook 'evil-emacs-state-exit-hook 'skk-mode)
-  )
-(leaf evil-matchit
-  :ensure t
-  :config (global-evil-matchit-mode 1))
-(leaf evil-leader
-  :ensure t
-  :config
-  ;; evil leader
-  (setq evil-leader/in-all-states 1)
-  (global-evil-leader-mode)
-  (evil-leader/set-leader "SPC")
-  (evil-leader/set-key
-    "SPC" 'execute-extended-command
-    ":" 'shell-command
-    "TAB" (lambda() (interactive)(other-window 1))
-    ;; "a" 'avy-goto-word-0
-    "b" 'counsel-ibuffer
-    "B" 'byte-compile-file
-    "C" (lambda() (interactive)(find-file "~/.emacs.d/init.el"))
-    "d" 'dired-jump
-    ;; "f" 'projectile-find-file
-    "f" 'counsel-find-file
-    "g" 'magit-status
-    "h" 'lsp-describe-thing-at-point
-    "j" 'skk-mode
-    "k" 'kill-buffer
-    "m" 'magit-status
-    "q" 'kill-buffer-and-window
-    "Q" (lambda () (interactive) (save-buffers-kill-emacs))
-    "r" 'helm-recentf
-    "s" 'eshell
-    "S" 'shell
-    "t" 'neotree-toggle
-    "T" 'treemacs
-    "w" 'save-buffer
-    "y" 'yas-insert-snippet
-    ;; "q" 'save-buffer-kill-terminal
-    "^" (lambda () (interactive)
-          (show-org-buffer)
-          (evil-normal-state))
-    ";" 'comment-line
-    "x" 'toggle-transparency
-    "0" 'delete-window
-    "1" 'delete-other-windows
-    "2" 'split-window-below
-    "3" 'split-window-right
-    "9" 'create-scratch-buffer
-    ))
+    (leaf magit
+      :ensure t
+      :config
+      (add-hook 'magit-mode-hook (lambda () (skk-mode)))
+      )
+    (leaf which-key
+      :ensure t
+      :config (which-key-mode)
+      :config (setq which-key-idle-delay 1000)
+      )
 
-(leaf magit
-  :ensure t
-  :config
-  (add-hook 'magit-mode-hook (lambda () (skk-mode)))
-  )
-(leaf which-key
-  :ensure t
-  :config (which-key-mode)
-  :config (setq which-key-idle-delay 1000)
-  )
-;; You can also configure builtin package via leaf!
-(leaf cus-start
-  :doc "define customization properties of builtins"
-  :tag "builtin" "internal"
-  :custom ((truncate-lines . t)
-           (menu-bar-mode . nil)
-           (tool-bar-mode . nil)
-           (scroll-bar-mode . nil)
-           (indent-tabs-mode . nil)
-           )
-  :config
-  ;; shut up, emacs!
-  ;; (setq default-input-method "japanese-skk")
-  (setq display-warning-minimum-level :error))
-(defun create-scratch-buffer
-    nil "create scratch buffer"
+    (leaf tree-sitter
+      :ensure t
+      :config
+      ;; activate tree-sitter on any buffer containing code for which it has a parser available
+      (global-tree-sitter-mode)
+      ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+      ;; by switching on and off
+      (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+    (leaf tree-sitter-langs
+      :ensure t
+      :after tree-sitter)
+    (leaf typescript-mode
+      :ensure t
+      :after tree-sitter
+      :config
+      ;; we choose this instead of tsx-mode so that eglot can automatically figure out language for server
+      ;; see https://github.com/joaotavora/eglot/issues/624 and https://github.com/joaotavora/eglot#handling-quirky-servers
+      (define-derived-mode typescriptreact-mode typescript-mode
+        "TypeScript TSX")
+
+      ;; use our derived mode for tsx files
+      (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
+      ;; by default, typescript-mode is mapped to the treesitter typescript parser
+      ;; use our derived mode to map both .tsx AND .ts -> typescriptreact-mode -> treesitter tsx
+      (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
+
+    (leaf quelpa-leaf
+      :ensure t
+      :config
+      (quelpa
+       '(quelpa-leaf
+         :fetcher git
+         :url "https://github.com/quelpa/quelpa-leaf.git"))
+      (require 'quelpa-leaf)
+      (quelpa-leaf-init)
+      )
+    ;; https://github.com/orzechowskid/tsi.el/
+    ;; great tree-sitter-based indentation for typescript/tsx, css, json
+    (leaf tsi
+      :after tree-sitter
+      :quelpa (tsi :fetcher github :repo "orzechowskid/tsi.el")
+      :quelpa (:upgrade nil)
+      ;; define autoload definitions which when actually invoked will cause package to be loaded
+      :commands (tsi-typescript-mode tsi-json-mode tsi-css-mode)
+      :init
+      (add-hook 'typescript-mode-hook (lambda () (tsi-typescript-mode 1)))
+      (add-hook 'json-mode-hook (lambda () (tsi-json-mode 1)))
+      (add-hook 'css-mode-hook (lambda () (tsi-css-mode 1)))
+      (add-hook 'scss-mode-hook (lambda () (tsi-scss-mode 1))))
+
+
+    (leaf org-journal
+      :ensure t
+      :config
+      (setq org-journal-dir "~/org/journal")
+      (setq work-directory "~/org/")
+      (setq memofile (concat work-directory "memo.org"))
+      (setq todofile (concat work-directory "TODO.org"))
+      (setq org-agenda-files `(,todofile ))
+      (setq org-capture-templates
+            '(
+              ("m" "メモ" entry (file+headline memofile "memo")
+               "** %?\n*** 参考\n\nEntered on %U\n %i\n %a\n")
+
+	      ("p" "プログラミングノート" entry (file+headline  memofile "Programming note")
+	       "** %? \n\n*** カテゴリ\n\n*** 内容\n\n\nEntered on %U\n %i\n %a\n")
+
+	      ("c" "チェックボックス" checkitem (file+headline   todofile "checkbox")
+	       "[ ] %? %U\n")
+	      ("t" "TODO" entry (file+headline todofile "ToDo")
+               "*** TODO [/] %?\n- [ ] \nCAPTURED_AT: %U\n %i\n")
+
+	      ("r" "調査内容" entry (file+headline memofile "Reserch")
+	       "** %?\nEntered on %U\n %i\n %a\n")
+
+	      ("S" "学習内容" entry (file+headline memofile "Study")
+	       "** %?\nEntered on %U\n %i\n %a\n")
+
+	      ("w" "単語帳" item (file+headline memofile "words")
+	       "- %?\nEntered on %U\n %i\n %a\n")
+
+	      ("W" "単語帳（複数語）" entry (file+headline memofile "words")
+	       "** %?\n - \nEntered on %U\n %i\n %a\n")
+
+
+              ("l" "記録" entry (file+headline memofile "Log")
+               "** %?\nEntered on %U\n %i\n %a\n")
+
+              ("s" "文章" entry (file+headline memofile "文章")
+               "** %?\nEntered on %U\n %i\n %a\n")
+
+	      ("i" "アイデア" entry (file+headline memofile "アイデア")
+               "* %?\nEntered on %U\n %i\n %a\n")
+
+
+	      ("b" "経済" entry (file+headline memofile "Business")
+               "** %?\nEntered on %U\n %i\n %a\n")
+
+              ("P" "Project" entry (file+headline memofile "Project")
+               "** %?\nEntered on %U\n %i\n")))
+      ))
+  ;; </leaf-install-code>
+
+  ;; Now you can use leaf!
+  (leaf leaf-tree :ensure t)
+  (leaf leaf-convert :ensure t)
+  (leaf transient-dwim
+    :ensure t
+    :bind (("M-=" . transient-dwim-dispatch)))
+
+  ;; You can also configure builtin package via leaf!
+  (leaf cus-start
+    :doc "define customization properties of builtins"
+    :tag "builtin" "internal"
+    :custom ((truncate-lines . t)
+             (menu-bar-mode . nil)
+             (tool-bar-mode . nil)
+             (scroll-bar-mode . nil)
+             (indent-tabs-mode . nil)
+             )
+    :config
+    ;; shut up, emacs!
+    ;; (setq default-input-method "japanese-skk")
+    (setq display-warning-minimum-level :error))
+  (defun create-scratch-buffer
+      nil "create scratch buffer"
+      (interactive)
+      (switch-to-buffer (get-buffer-create "*scratch*"))
+      (text-mode))  
+  (add-to-list 'exec-path (expand-file-name "~/.cargo.bin"))
+  (set-language-environment "UTF-8")
+  (global-set-key (kbd "C-c t") 'display-line-numbers-mode)
+  ;; emacs の起動画面を消す
+  ;; https://pcvogel.sarakura.net/2013/06/17/31151
+  (setq inhibit-startup-message t)
+  (setq initial-scratch-message nil)
+  ;;カーソルの点滅を消す
+  (blink-cursor-mode 0)
+  ;;括弧の自動補完
+  (electric-pair-mode 1)
+
+  (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+  (add-hook 'org-mode-hook 'display-line-numbers-mode)
+  ;; *scratch* buffer をテキストモードで開く
+  (setq initial-major-mode 'text-mode)
+  ;; scratch buffer をorg-modeで作成するmy-scratch-buffer
+  ;; https://emacs.stackexchange.com/questions/16492/is-it-possible-to-create-an-org-mode-scratch-buffer
+  ;; lawlistさん作
+  (defun my-scratch-buffer ()
+    "Create a new scratch buffer -- \*hello-world\*"
     (interactive)
-    (switch-to-buffer (get-buffer-create "*scratch*"))
-    (text-mode))  
-(add-to-list 'exec-path (expand-file-name "~/.cargo.bin"))
-(set-language-environment "UTF-8")
-(global-set-key (kbd "C-c t") 'display-line-numbers-mode)
-;; emacs の起動画面を消す
-;; https://pcvogel.sarakura.net/2013/06/17/31151
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
-;;カーソルの点滅を消す
-(blink-cursor-mode 0)
-;;括弧の自動補完
-(electric-pair-mode 1)
+    (let ((n 0)
+          bufname buffer)
+      (catch 'done
+        (while t
+          (setq bufname (concat "*my-scratch-org-mode"
+                                (if (= n 0) "" (int-to-string n))
+                                "*"))
+          (setq n (1+ n))
+          (when (not (get-buffer bufname))
+            (setq buffer (get-buffer-create bufname))
+            (with-current-buffer buffer
+              (org-mode))
+            ;; When called non-interactively, the `t` targets the other window (if it exists).
+            (throw 'done (display-buffer buffer t))) ))))
+  ;; 警告音もフラッシュも全て無効(警告音が完全に鳴らなくなるので注意)
+  (setq ring-bell-function 'ignore)
 
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-(add-hook 'org-mode-hook 'display-line-numbers-mode)
-;; *scratch* buffer をテキストモードで開く
-(setq initial-major-mode 'text-mode)
-;; scratch buffer をorg-modeで作成するmy-scratch-buffer
-;; https://emacs.stackexchange.com/questions/16492/is-it-possible-to-create-an-org-mode-scratch-buffer
-;; lawlistさん作
-(defun my-scratch-buffer ()
-  "Create a new scratch buffer -- \*hello-world\*"
-  (interactive)
-  (let ((n 0)
-        bufname buffer)
-    (catch 'done
-      (while t
-        (setq bufname (concat "*my-scratch-org-mode"
-                              (if (= n 0) "" (int-to-string n))
-                              "*"))
-        (setq n (1+ n))
-        (when (not (get-buffer bufname))
-          (setq buffer (get-buffer-create bufname))
-          (with-current-buffer buffer
-            (org-mode))
-          ;; When called non-interactively, the `t` targets the other window (if it exists).
-          (throw 'done (display-buffer buffer t))) ))))
-;; 警告音もフラッシュも全て無効(警告音が完全に鳴らなくなるので注意)
-(setq ring-bell-function 'ignore)
-
-(leaf tree-sitter
-  :ensure t
-  :config
-  ;; activate tree-sitter on any buffer containing code for which it has a parser available
-  (global-tree-sitter-mode)
-  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
-  ;; by switching on and off
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
-
-(leaf tree-sitter-langs
-  :ensure t
-  :after tree-sitter)
-(leaf typescript-mode
-  :ensure t
-  :after tree-sitter
-  :config
-  ;; we choose this instead of tsx-mode so that eglot can automatically figure out language for server
-  ;; see https://github.com/joaotavora/eglot/issues/624 and https://github.com/joaotavora/eglot#handling-quirky-servers
-  (define-derived-mode typescriptreact-mode typescript-mode
-    "TypeScript TSX")
-
-  ;; use our derived mode for tsx files
-  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
-  ;; by default, typescript-mode is mapped to the treesitter typescript parser
-  ;; use our derived mode to map both .tsx AND .ts -> typescriptreact-mode -> treesitter tsx
-  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
-
-(leaf quelpa-leaf
-  :ensure t
-  :config
-  (quelpa
-   '(quelpa-leaf
-     :fetcher git
-     :url "https://github.com/quelpa/quelpa-leaf.git"))
-  (require 'quelpa-leaf)
-  (quelpa-leaf-init)
-  )
-;; https://github.com/orzechowskid/tsi.el/
-;; great tree-sitter-based indentation for typescript/tsx, css, json
-(leaf tsi
-  :after tree-sitter
-  :quelpa (tsi :fetcher github :repo "orzechowskid/tsi.el")
-  :quelpa (:upgrade nil)
-  ;; define autoload definitions which when actually invoked will cause package to be loaded
-  :commands (tsi-typescript-mode tsi-json-mode tsi-css-mode)
-  :init
-  (add-hook 'typescript-mode-hook (lambda () (tsi-typescript-mode 1)))
-  (add-hook 'json-mode-hook (lambda () (tsi-json-mode 1)))
-  (add-hook 'css-mode-hook (lambda () (tsi-css-mode 1)))
-  (add-hook 'scss-mode-hook (lambda () (tsi-scss-mode 1))))
-
-
-(leaf org-journal
-  :ensure t
-  :config
-  (setq org-journal-dir "~/org/journal")
-  (setq work-directory "~/org/")
-  (setq memofile (concat work-directory "memo.org"))
-  (setq todofile (concat work-directory "TODO.org"))
-  (setq org-agenda-files `(,todofile ))
-  (setq org-capture-templates
-        '(
-          ("m" "メモ" entry (file+headline memofile "memo")
-           "** %?\n*** 参考\n\nEntered on %U\n %i\n %a\n")
-
-	  ("p" "プログラミングノート" entry (file+headline  memofile "Programming note")
-	   "** %? \n\n*** カテゴリ\n\n*** 内容\n\n\nEntered on %U\n %i\n %a\n")
-
-	  ("c" "チェックボックス" checkitem (file+headline   todofile "checkbox")
-	   "[ ] %? %U\n")
-	  ("t" "TODO" entry (file+headline todofile "ToDo")
-           "*** TODO [/] %?\n- [ ] \nCAPTURED_AT: %U\n %i\n")
-
-	  ("r" "調査内容" entry (file+headline memofile "Reserch")
-	   "** %?\nEntered on %U\n %i\n %a\n")
-
-	  ("S" "学習内容" entry (file+headline memofile "Study")
-	   "** %?\nEntered on %U\n %i\n %a\n")
-
-	  ("w" "単語帳" item (file+headline memofile "words")
-	   "- %?\nEntered on %U\n %i\n %a\n")
-
-	  ("W" "単語帳（複数語）" entry (file+headline memofile "words")
-	   "** %?\n - \nEntered on %U\n %i\n %a\n")
-
-
-          ("l" "記録" entry (file+headline memofile "Log")
-           "** %?\nEntered on %U\n %i\n %a\n")
-
-          ("s" "文章" entry (file+headline memofile "文章")
-           "** %?\nEntered on %U\n %i\n %a\n")
-
-	  ("i" "アイデア" entry (file+headline memofile "アイデア")
-           "* %?\nEntered on %U\n %i\n %a\n")
-
-
-	  ("b" "経済" entry (file+headline memofile "Business")
-           "** %?\nEntered on %U\n %i\n %a\n")
-
-          ("P" "Project" entry (file+headline memofile "Project")
-           "** %?\nEntered on %U\n %i\n")))
   
   (global-set-key (kbd "C-c C-j") 'org-journal-new-entry)
   ;; https://www.mhatta.org/wp/2018/08/16/org-mode-101-1/
@@ -476,12 +489,6 @@
 ;;     (define-key copilot-mode-map (kbd "<tab>") #'my/copilot-tab))
 ;;   )
 
-(leaf beacon
-  :ensure t
-  :config (beacon-mode 1) (setq beacon-color "green"))
-
-(leaf ddskk
-  :ensure t)
 
 ;; ;; ↑package setting end
 
